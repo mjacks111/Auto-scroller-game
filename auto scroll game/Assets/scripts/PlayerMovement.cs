48e1baeb95a9087d7ed8a22ceeb34a632df145d2
Marcus Jackson <mjacks111@users.noreply.github.com>
2017-09-06 21:55:52 -0700

    Initial commit
    setting up the player, ground field, and working on camera script

---
+++ auto scroll game/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update ()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
